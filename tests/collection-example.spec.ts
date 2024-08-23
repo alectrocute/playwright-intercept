ccc173da4dd5451230fad97d5deb3492f68e6c5a
Alec Armbruster <35377827+alectrocute@users.noreply.github.com>
2024-08-23 16:03:57 -0700

    update collection-example.spec.ts

--- tests/collection-example.spec.ts
+++ tests/collection-example.spec.ts
@@ -24,16 +24,6 @@ export async function intercept({ page }, use) {
         fixturePathPrefix: path.join(process.cwd(), "tests"),
     });
 
-    // You could treat this like a global "before each" hook
-    const mockAppGet = intercept.get({
-        url: 'https://example.com',
-        fixture: 'mock-app/index.html',
-    });
-
-    await page.goto('https://example.com');
-
-    await mockAppGet.wait();
-
     await use(intercept);
 }
 
@@ -58,12 +48,20 @@ const fixtures = {
 const test = base.test.extend<Fixtures>(fixtures);
 
 // Let's now use it, pulling in the `mockItemEndpoints` fixture.
-test('Example using collection of intercepts', async ({ page, mockItemEndpoints }) => {
+test('Example using collection of intercepts', async ({ page, intercept, mockItemEndpoints }) => {
+    const mockDemoAppGet = intercept.get({
+        url: 'https://example.com',
+        fixture: 'mock-app/index.html', // Yes, you can even mock HTML responses!
+    });
+
+    await page.goto('https://example.com');
+
+    await mockDemoAppGet.wait();
+
     await page.locator('#url').fill("/get-all-items");
     await page.locator('#method').selectOption('GET');
     await page.locator('#submit').click();
 
-    // See? You've already got your intercepts set up!
     await mockItemEndpoints.getAllItems.wait();
 
     await page.locator('#url').fill("/create-item");
@@ -71,4 +69,7 @@ test('Example using collection of intercepts', async ({ page, mockItemEndpoints 
     await page.locator('#submit').click();
 
     await mockItemEndpoints.createItem.wait();
+
+    // We've now tested that our frontend app is correctly making requests 
+    // to both `GET /get-all-items` and `POST /create-item` endpoints.
 });
